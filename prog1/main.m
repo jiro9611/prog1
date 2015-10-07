97f39f9f3adb12a7eaddec478c64ab3e5004f7a9
Jiro Lin <jiro.lin9611@gmail.com>
2015-10-07 21:27:35 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by jiro9611 on 10/7/15.
+//  Copyright © 2015 jiro9611. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
