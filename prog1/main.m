d4609cd9be5fe80ff834977a65c8570b507c5926
Jiro Lin <jiro.lin9611@gmail.com>
2015-10-07 22:29:34 +0800

    objective-c practice

--- prog1/main.m
+++ prog1/main.m
@@ -8,10 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+//---- @interface section ----
+
+@interface Fraction: NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+//---- @implementation section ----
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void) print
+{
+    NSLog (@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+//---- program section ----
+
+int main (int argc, char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction;
+        
+        // Create an instance of a Fraction
+        
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // Set fraction to 1/3
+        
+        [myFraction setNumerator: 2];
+        [myFraction setDenominator: 5];
+        
+        // Display the fraction using the print method
+        
+        NSLog (@"The value of myFraction is:");
+        [myFraction print];
     }
     return 0;
 }
